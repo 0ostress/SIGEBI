e4114e854eb7bbc09392bc68b45a3a6e8265f540
0ostress <emilstudios21qgmail.com>
2026-06-06 22:46:13 -0400

    Agregar archivos de proyecto.

---
+++ SGA.Domain/Entities/Configuration/Ruta.cs
@@ -0,0 +1,11 @@
+﻿namespace SGA.Domain.Entities.Configuration
+{
+    public class Ruta
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Origen { get; set; } = string.Empty;
+        public string Destino { get; set; } = string.Empty;
+        public string Estado { get; set; } = "Activo";
+    }
+}
